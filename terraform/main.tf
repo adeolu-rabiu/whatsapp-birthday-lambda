4761b98e92f444e133e29625e6cb36d9ebf2b360
adeolu rabiu <adeolu.rabiu@gmail.com>
2025-03-19 05:06:12 +0000

    Add S3 static website bucket for Birthday Bot UI, Create S3 bucket for hosting the React application , Configure website hosting settings for Single Page Application , Set up public access permissions and bucket policy, Enable website URL output in Terraform, Fix deployment pipeline to use proper S3 bucket path

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Get current AWS account ID
+data "aws_caller_identity" "current" {}
+
 # DynamoDB Tables
 resource "aws_dynamodb_table" "birthdays_table" {
   name         = "Birthdays"
@@ -51,4 +54,55 @@ resource "aws_dynamodb_table" "whatsapp_groups_table" {
     Name        = "WhatsAppGroups"
     Environment = var.environment
   }
+}
+
+# S3 Bucket for Web UI
+resource "aws_s3_bucket" "web_ui_bucket" {
+  bucket = "birthday-bot-ui-${data.aws_caller_identity.current.account_id}"
+
+  tags = {
+    Name        = "Birthday Bot UI"
+    Environment = var.environment
+  }
+}
+
+# Enable website hosting
+resource "aws_s3_bucket_website_configuration" "web_ui_website" {
+  bucket = aws_s3_bucket.web_ui_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"  # For SPAs, route all errors back to index.html
+  }
+}
+
+# Make the bucket public
+resource "aws_s3_bucket_public_access_block" "web_ui_access" {
+  bucket = aws_s3_bucket.web_ui_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Bucket policy for public read access
+resource "aws_s3_bucket_policy" "web_ui_policy" {
+  bucket = aws_s3_bucket.web_ui_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.web_ui_bucket.arn}/*"
+      },
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.web_ui_access]
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,6 @@
 #   value = aws_apigatewayv2_api.birthday_api.api_endpoint
 # }
 
-# output "web_ui_url" {
-#   value = "http://${aws_s3_bucket.web_ui_bucket.bucket}.s3-website-${var.aws_region}.amazonaws.com"
-# }
+ output "web_ui_url" {
+   value = "http://${aws_s3_bucket.web_ui_bucket.bucket}.s3-website-${var.aws_region}.amazonaws.com"
+ }
